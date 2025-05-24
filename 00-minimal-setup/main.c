cd68875a6899e9eef632a327834b67e87a5aa75c
Delmainn <134325371+Delmainn@users.noreply.github.com>
2025-05-24 15:47:29 -0400

    Interacting with the GPIOA MODER register by manipulating the bit values to toggle between input mode and output mode. Barebones.

---
+++ 00-minimal-setup/main.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+int main(){
+
+    * (volatile uint32_t *) (0x40020000 + 0) = 0; // Setting A0-A15 to input (00) mode.
+
+    * (volatile uint32_t *) (0x40020000 + 0) &= ~(3 << 6); // Clear bit range 6-7
+    * (volatile uint32_t *) (0x40020000 + 0) |= 1 << 6;   // Set bit range 6-7 to 1
+
+
+
+
+    return 0;
+}
