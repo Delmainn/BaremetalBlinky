3ff26a33a8262e349aa1dc8dba56298313c2c09e
Delmainn <134325371+Delmainn@users.noreply.github.com>
2025-05-28 17:58:03 -0400

    Took a step in making the code less cryptic and more readable by employing functions and variables. Allows that we can see what port and Bank we are working with in the GPIO Peripheral. Need shorter commits and more refined explanations for my changes though. '_'

--- 00-minimal-setup/main.c
+++ 00-minimal-setup/main.c
@@ -1,37 +1,39 @@
 #include <stdint.h>
-
+/* Creating a struct to concpetualise the GPIO peripheral into its components registers as is defined
+in the datasheet*/
 struct gpio {
     volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
 };
 
-#define GPIOA ((struct gpio *) 0x40020000)
-
+//Macros to refrence any pin and bank(port) on the GPIO peripheral in a consistent way
 #define GPIO(bank)  ((struct gpio *) (0x40020000 + 0x400 * (bank)))
 
-#define PIN (bank, num)  ((((bank)- 'A') << 8) | (num))
-#define PINN0(pin) (pin & 255)
+#define PIN(bank, num) ((((bank)- 'A') << 8) | (num))
+#define PINNO(pin) (pin & 255)
 #define PINBANK(pin) (pin >> 8)
 
 
-enum {
-    GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG};
 
-static inline void gpio_set_mode(struct gpio *gpio, uint8_t pin, uint8_t mode) {
-    gpio -> MODER &= ~(3U << (pin * 2));
-    gpio -> MODER |= (mode & 3) << (pin * 2);
+
+enum {
+    GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG};  // Reminds me of PinMode in Arduino
+/* Function that turns pin value to actual hardware configuration - manipulating the bits*/
+static inline void gpio_set_mode(uint16_t pin, uint8_t mode) {
+    struct gpio *gpio = GPIO(PINBANK(pin)); //GPIO BANK
+    uint8_t n = PINNO(pin); //PIN NUMBER
+    gpio -> MODER &= ~(3U << (n * 2));   // clears existing setting (bits)
+    gpio -> MODER |= (mode & 3) << (n * 2); // Set new mode
 
 }
 
 
+
 int main(){
 
-    /* * (volatile uint32_t *) (0x40020000 + 0) = 0; // Setting A0-A15 to input (00) mode.
-    * (volatile uint32_t *) (0x40020000 + 0) &= ~(3 << 6); // Clear bit range 6-7
-    * (volatile uint32_t *) (0x40020000 + 0) |= 1 << 6;   // Set bit range 6-7 to 1 -> MODER[3] = 01 (OUTPUT)*/
-
-    // Making the code cleaner and improving readability.
-
-    gpio_set_mode(GPIOA, 3 /*pin*/, GPIO_MODE_OUTPUT); //Setting A3 to output mode
+    // Making the code cleaner and improving readability by calling aforemade functions
+    uint16_t pin = PIN('A', 3);
+    //uint16_t pin2 = PIN('G', 11); 
+    gpio_set_mode(pin, GPIO_MODE_OUTPUT); //Setting A3 to output mode
 
 
     return 0;
