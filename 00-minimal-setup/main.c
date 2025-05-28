a07463fd94b7c3b8b16c0d6fc0333899fb247066
Delmainn <134325371+Delmainn@users.noreply.github.com>
2025-05-28 12:45:27 -0400

    made a workspace to work on my project, while being able to reference my resources in the same folder on vscode. Add the config to gitignore. Added a comment in the main.c.

--- 00-minimal-setup/main.c
+++ 00-minimal-setup/main.c
@@ -5,7 +5,7 @@ int main(){
     * (volatile uint32_t *) (0x40020000 + 0) = 0; // Setting A0-A15 to input (00) mode.
 
     * (volatile uint32_t *) (0x40020000 + 0) &= ~(3 << 6); // Clear bit range 6-7
-    * (volatile uint32_t *) (0x40020000 + 0) |= 1 << 6;   // Set bit range 6-7 to 1
+    * (volatile uint32_t *) (0x40020000 + 0) |= 1 << 6;   // Set bit range 6-7 to 1 -> MODER[3] = 01 (OUTPUT)
 
 
 
